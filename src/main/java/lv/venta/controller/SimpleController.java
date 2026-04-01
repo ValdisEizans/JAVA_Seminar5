6a15b4e28af5f14c45623f5089ebbcb6f8d70797
ValdisEizans <valdis.eizans@gmail.com>
2026-04-01 11:14:55 +0300

    Edit: new controller with data

--- src/main/java/lv/venta/controller/SimpleController.java
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -1,6 +1,9 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,15 @@ public class SimpleController {
 	public String getShowPage() {
 		System.out.println("Mans pirmais kontrolieris");
 		return "show-page";
+	}
+	
+	@GetMapping("/data")//localhost:8080/simple/data
+	public String getDataInPage(Model model) {
+		System.out.println("Izpildas datu kontrolieris");
+		Random rand = new Random();
+		String data = "@Valdis " + rand.nextInt(2010,2026);
+		model.addAttribute("package",data);
+		return "show-data-page";
 	}
 
 }
