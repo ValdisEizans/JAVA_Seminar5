2425ffec1520da891a25e633493f117de1340585
ValdisEizans <valdis.eizans@gmail.com>
2026-04-01 11:50:36 +0300

    Edit: model created

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,6 +1,15 @@
 package lv.venta.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//ToString
 public class Product {
+	//SSetter(value = AccessLevel.NONE)
 	//Mainigie
 	private long id;
 	private String title; 
@@ -9,16 +18,77 @@ public class Product {
 	private String description;
 	private ProductType productType;
 	
-	//Geteri
+	private static long counter = 0;
 	
-	
-	
-	//Seteri
-	
-	
-	//Konstruktori
-	
-	
-	//toString
+	//Geteri - nak no Lombok
+	//No Maven repository pom faila pieviento Lombok
 
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public ProductType getProductType() {
+		return productType;
+	}
+	
+	//Seteri -  nak no Lombok
+	
+	
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setProductType(ProductType productType) {
+		this.productType = productType;
+	}
+	
+	//Bezaurgumenta konstruktori -  nak no Lombok
+	
+	//Argumenta konstruktori -  nak no Lombok
+	public Product(String inputTitle, float inputPrice, int inputQuantity, String inputDescription, ProductType inputProductType ) {
+		setTitle(inputTitle);
+		setDescription(inputDescription);
+		setPrice(inputPrice);
+		setQuantity(inputQuantity);
+		setProductType(inputProductType);
+	}
+	//toString
+	@Override
+	public String toString() {
+		String result = id + " " + title + " " + description + " " + price + " " + quantity;
+		return result;
+	}
 }
