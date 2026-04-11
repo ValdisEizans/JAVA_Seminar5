2337358b1ede07955f2936af578813264586574a
ValdisEizans <valdis.eizans@gmail.com>
2026-04-11 11:22:11 +0300

    Edit: added commandline runner

--- src/main/java/lv/venta/JavaSeminar5Application.java
+++ src/main/java/lv/venta/JavaSeminar5Application.java
@@ -1,13 +1,37 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.model.Product;
+import lv.venta.model.ProductType;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class JavaSeminar5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSeminar5Application.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner testRepo(IProductRepo productRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Product prod1 = new Product("Abols", 6.95f, 5, "Garsigs", ProductType.fruit);
+				Product prod2 = new Product("Burkans", 0.99f, 5, "Super", ProductType.vegetable);
+				Product prod3 = new Product("Biete", 1.05f, 5, "Negarsigs", ProductType.vegetable);				
+				
+				productRepo.save(prod1);
+				productRepo.save(prod2);
+				productRepo.save(prod3);
+				
+			}
+		};
 	}
 
 }
