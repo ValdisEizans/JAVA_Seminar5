d6d33516e9778638aad72c810d35c158183ccee4
ValdisEizans <valdis.eizans@gmail.com>
2026-04-15 12:14:27 +0300

    Edit: controller add

---
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -0,0 +1,31 @@
+package lv.venta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import lv.venta.model.Product;
+import lv.venta.service.IProductCRUDService;
+
+@Controller
+@RequestMapping("/product/crud")
+public class ProductCRUDController {
+	
+	@Autowired
+	private IProductCRUDService prodService;
+	
+	@GetMapping("/all")//localhost:8080//product/crud/all
+	public String getAllProducts(Model model) {
+		try {
+			model.addAttribute("package", prodService.retrieveAll());
+			return "show-all-products-page";
+		}
+		catch (Exception e) {
+			return "error-page";
+		}
+
+	}
+}
